e6045d0e9aaacda7e1f7173a3d5849bd6d51268e
PowerMover <85679541+PowerMover@users.noreply.github.com>
2021-06-17 15:08:57 +0300

    Добавил счетчик лаков

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ let themeButton = document.querySelector('.theme-button');
 themeButton.onclick = function () {
    page.classList.toggle ('dark-theme');
 }
+let heart = document.querySelector('.heart');
+let likesNumber = document.querySelector('.likes-number');
+let counter = 0;
+
+heart.onclick = function () {
+  if (heart.classList.contains('added')) {
+    counter--;
+  } else {
+    counter++;
+  }
+  likesNumber.textContent = counter;
+  heart.classList.toggle('added');
+};
